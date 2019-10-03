1462e1a902351635313d5dc835315234fa2267b4
Wiktor Czajkowski <wiktor.czajkowski@gmail.com>
2019-10-03 20:59:45 +0200

    ex 1.11

--- 1-11-recursive-iterative.scm
+++ 1-11-recursive-iterative.scm
@@ -1,11 +1,18 @@
-(define (f n)
-  (if (< n 3)
-    n
-    (+ (f (- n 1))
-       (* 2 (f (- n 2)))
-       (* 3 (f (- n 3))))))
+(define (>= a b) (or (> a b) (= a b)))
 
-(define (f n)
-  (if (< n 3)
-    n
-    ))
+(define (f-recursive n)
+  (if (< n 3) 
+      n
+      (+ (f-recursive (- n 1)) 
+         (* 2 (f-recursive (- n 2)))
+         (* 3 (f-recursive (- n 3))))))
+
+(define (f-iter n)
+  (define (iter a b c counter)
+    (cond ((< n 3) n)
+          ((= counter (- n 1)) c)
+          (else (iter b 
+                      c 
+                      (+ c (* 2 b) (* 3 a)) 
+                      (+ counter 1)))))
+  (iter 0 1 2 1))
