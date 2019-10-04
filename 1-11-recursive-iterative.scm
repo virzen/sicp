3496d9bf8a2adc68d0a9dd124c0245075864e1e9
Wiktor Czajkowski <wiktor.czajkowski@gmail.com>
2019-10-04 09:58:50 +0200

    add notes to ex 1-11

--- 1-11-recursive-iterative.scm
+++ 1-11-recursive-iterative.scm
@@ -1,3 +1,22 @@
+; One difference between recursive-process and iterative-process versions seems
+; to be that recursive-process one seems start from the top, so "here is the
+; expression that gives the final result", whereas iterative-process start at the
+; first known, defined value and builds the final expression from that.
+;
+; I found the iterative-process approach to include a concept of a "tail" (not 
+; sure if related to tail recursion). In each iteration we calcuate another value 
+; that gets us closer to the result and we _keep previous values_ that will be yet 
+; necessary. In this particular example, in each iteration we need three previous 
+; results, so we have 3 state variables for that. With Fibonacci, we used two of the
+; the previous ones, so kept two of them. In factorial—one. 
+;
+; However, the fact that formula references some variables is not the deciding factor.
+; If in this exercise it only referenced f(n - 1) and f(n - 3), we would still need to
+; keep three variables. That's because what is f(n - 2) in one iteration, becomes f(n - 3)
+; on another, and so we also need to keep track of f(n - 2). This seems to imply that 
+; the tail must include all the results _up to_ the last result we're gonna need in each 
+; iteration.
+
 (define (f n)
   (if (< n 3) 
       n
