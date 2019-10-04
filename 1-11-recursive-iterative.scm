70b7bc2801323ec70b748a7affd4fa578bdf2df5
Wiktor Czajkowski <wiktor.czajkowski@gmail.com>
2019-10-04 09:46:32 +0200

    add iterative version without lexical scoping to ex 1-11

--- 1-11-recursive-iterative.scm
+++ 1-11-recursive-iterative.scm
@@ -5,6 +5,7 @@
          (* 2 (f (- n 2)))
          (* 3 (f (- n 3))))))
 
+; using lexical scope not to have to pass n around
 (define (f-iter n)
   (define (iter a b c counter)
     (cond ((< n 3) n)
@@ -14,3 +15,15 @@
                       (+ c (* 2 b) (* 3 a)) 
                       (+ counter 1)))))
   (iter 0 1 2 1))
+
+; without lexical scoping
+(define (f n) (iter 0 1 2 1 n))
+
+(define (iter a b c counter n)
+  (cond ((< n 3) n)
+        ((= counter (- n 1)) c)
+        (else (iter b 
+                    c 
+                    (+ c (* 2 b) (* 3 a)) 
+                    (+ counter 1)
+                    n))))
